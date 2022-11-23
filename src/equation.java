b39f43860fa85b0997db0640b8bbbf69fc9a3087
Dexter <gusevsergey85@gmail.com>
2022-11-23 12:30:20 +0300

    Доделал уравнение

--- src/equation.java
+++ src/equation.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
 public class equation {
     public static void equality(){
-        for (int i = 1; i < 70; i++) {
-            for (int j = 1; j < 70; j++) {
-                if ((2 * i + j * 5) == 69){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите коэффициенты уравнения q, w, e: ");
+        boolean flag = false;
+        System.out.println("Введите q: ");
+        String num1 = scan.next();
+        System.out.println("Введите w: ");
+        String num2 = scan.next();
+        System.out.println("Введите e: ");
+        String num3 = scan.next();
+        int q = Integer.parseInt(num1);
+        int w = Integer.parseInt(num2);
+        int e = Integer.parseInt(num3);
+        for (int i = 1; i < e; i++) {
+            for (int j = 1; j < e; j++) {
+                if ((q * i + j * w) == e){
                     System.out.printf("Корни уравнения %d, %d\n", i, j);
+                    flag = true;
                 }
-
             }
+        }
+        if (!flag){
+            System.out.println("Корней нет");
         }
     }
 }
